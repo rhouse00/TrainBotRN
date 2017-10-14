b098813d94f4a6dd16cc559392bb52a944d71d53
rhouse00 <ryanjameshouse@gmail.com>
2017-10-14 09:07:10 -0700

    adds basic Router scenes and firebase access

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 import reducers from './reducers';
+import Router from './Router';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyA4PO7b2FwZriRNRFVjnDTTjVYh9rzhSAI",
+            authDomain: "trainbotrn.firebaseapp.com",
+            databaseURL: "https://trainbotrn.firebaseio.com",
+            projectId: "trainbotrn",
+            storageBucket: "trainbotrn.appspot.com",
+            messagingSenderId: "689544885582"
+          };
+          firebase.initializeApp(config);
+    }
     render() {
-        const store = createStore(reducers, {});
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
         const { mainPageStyle } = styles;
+
         return (
             <Provider store={store}>
-                <View style={mainPageStyle}>
-                    <LoginForm />
-                </View>
+                <Router />
             </Provider>
         );
     }
--- src/Router.js
+++ src/Router.js
@@ -1,3 +1,71 @@
+import React from 'react';
+import { Scene, Stack, Router, Actions } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+import WorkoutList from './components/WorkoutList';
+import Profile from './components/Profile';
+import TabIcon from './components/TabIcon';
+import Chat from './components/Chat';
+
+const RouterComponent = () => {
+    const { navBarStyle, navBarTitle, tabBarStyle } = styles;
+    return (
+        <Router navigationBarStyle={navBarStyle} titleStyle={navBarTitle}>
+            <Stack key='root' hideNavBar>
+                <Scene key='auth' initial>
+                    <Scene
+                        key='login'
+                        component={LoginForm}
+                        title='Login / Sign-up'
+                    />
+                </Scene>
+                <Scene 
+                    key='clientArea' 
+                    tabs
+                    tabBarStyle={tabBarStyle}
+                    showLabel={false}
+                >
+                    
+                    <Scene
+                        icon={TabIcon}
+                        key='profile'
+                        component={Profile}
+                        title='Profile'
+                    />
+                    <Scene
+                        key='workout'
+                        icon={TabIcon}
+                        component={WorkoutList}
+                        title="Today's Workout"
+                        initial
+                    />
+                    <Scene
+                        icon={TabIcon}
+                        key='chat'
+                        component={Chat}
+                        title='Chat'
+                    />
+                </Scene>
+            </Stack>
+        </Router>
+    );
+};
+
+const styles = {
+    navBarStyle: {
+        backgroundColor: "#c1ffff",
+        height: 60
+    },
+    navBarTitle:{
+        color:'#aaa'
+    },
+    tabBarStyle: {
+        backgroundColor: '#c1ffff',
+        height: 60
+    }
+}
+
+export default RouterComponent;
+
 
 // Login Page
     // Sign In
@@ -24,3 +92,4 @@
     // create new message
     // *** icebox ***
         // share video
+
