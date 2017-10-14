91e92c34ec8fc3c482b1edb3303265a5f7509fca
rhouse00 <ryanjameshouse@gmail.com>
2017-10-14 09:37:25 -0700

    adds custom tab navigation icons to RNRF

---
+++ src/images/index.js
@@ -0,0 +1,7 @@
+export const PINK_GLOVES = require('./gloves_pink_16x16.png');
+export const DUMBBELL = require('./dumbbell_unfilled_48x48.png');
+export const ACCOUNT_UNFILLED = require('./account_unfilled_48x48.png');
+export const ACCOUNT_FILLED = require('./account_filled_32x32.png');
+export const CHAT_FILLED_SQUARE = require('./chat_filled_square_48x48.png');
+export const CHAT_UNFILLED_DOUBLE = require('./chat_unfilled_two_bubble_48x48.png');
+export const CHAT_LINED_DOUBLE = require('./chat_lined_two_bubble_14x16.png');
