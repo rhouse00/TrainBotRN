cb627022aa16c95f5c8184372ce7710fbf640279
rhouse00 <ryanjameshouse@gmail.com>
2017-10-14 09:40:05 -0700

    adds reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import WorkoutReducer from './WorkoutReducer';
 
 export default combineReducers({
-    auth: AuthReducer
+    auth: AuthReducer,
+    workouts: WorkoutReducer
 });
