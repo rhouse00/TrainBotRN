9df20b3159d122a7a7c616ce52795e2feb754ce4
rhouse00 <ryanjameshouse@gmail.com>
2018-04-16 09:58:46 -0700

    adds Client and ClientFormData to combined reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
 import WorkoutReducer from './WorkoutReducer';
+import ClientReducer from './ClientReducer';
+import ClientFormReducer from './ClientFormReducer';
 
 export default combineReducers({
     auth: AuthReducer,
-    workouts: WorkoutReducer
+    workouts: WorkoutReducer,
+    client: ClientReducer,
+    clientFormData: ClientFormReducer
 });
