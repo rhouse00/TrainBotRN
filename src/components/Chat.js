6c2156583629c221254835a4d1ed8b4be5b712d6
rhouse00 <ryanjameshouse@gmail.com>
2018-04-22 23:43:42 -0700

    starts work on messaging feature

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class Chat extends Component {
+    componentWillMount() {
+        this.props.fetchMessages(); // need to create
+    }
+
     render() {
         return (
             <View>
