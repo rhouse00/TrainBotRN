85ef02c491c15d26ba4aa501c84e79319d9fc0c3
rhouse00 <ryanjameshouse@gmail.com>
2018-04-24 23:17:06 -0700

    adds button for submitting message

--- src/components/AddMessage.js
+++ src/components/AddMessage.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 // import { View, Text } from 'react-native';
-import { CardSection, Input } from './common';
+import { CardSection, Input, Button, Card } from './common';
 
 class AddMessage extends Component {
 
+   onSubmitButtonPress() {
+      console.log('Message Submitted');
+   }
+
    render() {
       return (
-         <CardSection>
-            <Input
-               placeholder='Add message...'
-            />
-         </CardSection>
+         <Card>
+            <CardSection>
+               <Input
+                  placeholder='Add message...'
+               />
+            </CardSection>
+            <CardSection>
+               <Button onPress={this.onSubmitButtonPress.bind(this)}>
+                  Send
+               </Button>
+            </CardSection>
+         </Card>
       );
    }
 }
