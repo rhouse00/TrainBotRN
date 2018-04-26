dcb94cc4b9a31e4bc853a4311ce70032ca8e58eb
rhouse00 <ryanjameshouse@gmail.com>
2018-04-25 22:43:46 -0700

    adds chat reducer to combined index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import AuthReducer from './AuthReducer';
 import WorkoutReducer from './WorkoutReducer';
 import ClientReducer from './ClientReducer';
 import ClientFormReducer from './ClientFormReducer';
+import ChatMessageForm from './ChatFormReducer';
 
 export default combineReducers({
     auth: AuthReducer,
     workouts: WorkoutReducer,
+    chatMessageForm: ChatMessageForm,
     client: ClientReducer,
     clientFormData: ClientFormReducer
 });
